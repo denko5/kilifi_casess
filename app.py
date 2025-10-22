119a2089e80948c7d4e08f49d0569f35bfe80cdc
Denis <deniskombe5@gmail.com>
2025-10-22 10:28:43 +0300

    Fixed database URI for Railway

--- app.py
+++ app.py
@@ -801,7 +801,12 @@ def bulk_schedule():
 
 
 
+# if __name__ == '__main__':
+#     with app.app_context():
+#         db.create_all()
+#     app.run(host="0.0.0.0", port=5001, debug=True)
+
+
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    app.run(host="0.0.0.0", port=5001, debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
